563cee41e132794cd30e543e3e2af70d84cbc009
Hecktagon <tayler.c.hilton@gmail.com>
2025-09-10 13:45:22 -0600

    working on move calculator logic.

--- shared/src/main/java/chess/CalculatorHelper.java
+++ shared/src/main/java/chess/CalculatorHelper.java
@@ -1,5 +1,28 @@
 package chess;
 
+import java.util.Collection;
+
 public class CalculatorHelper {
 
+    // takes in a direction [x, y]/[col increment,row increment], and returns a ChessMove if move is possible
+    static ChessMove checkMove(int[] direction, ChessPosition myPos, ChessBoard board, ChessPiece.PieceType promo){
+        ChessMove move = null;
+        int myCol = myPos.getColumn();
+        int myRow = myPos.getRow();
+
+        int newCol = myCol + direction[0];
+        int newRow = myRow + direction[1];
+
+        ChessPosition newPos = new ChessPosition(newRow, newCol);
+        ChessPiece targetPiece = board.getPiece(newPos);
+
+        // if there is no piece, it is a valid move. if there is a piece, only valid if can take.
+        if (targetPiece == null){
+            return new ChessMove(myPos, newPos, promo);
+        } else {
+
+        }
+
+        return move;
+    }
 }
