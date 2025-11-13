9da42f3945160b83b2966c13ab5a6bf19e9ee1eb
Hecktagon <tayler.c.hilton@gmail.com>
2025-11-13 14:22:35 -0700

    Added pieceToUI helper function to GameUI

--- client/src/main/java/ui/GameUI.java
+++ client/src/main/java/ui/GameUI.java
@@ -2,15 +2,19 @@ package ui;
 
 import chess.ChessBoard;
 import chess.ChessGame;
+import chess.ChessPiece;
 
 import static ui.EscapeSequences.*;
 
 public class GameUI {
-    private static final String[] columns = {"a", "b", "c", "d", "e", "f", "g", "h"};
-    private static final String[] rows = {"1", "2", "3", "4", "5", "6", "7", "8"};
-    private static final String BLACKBG = SET_BG_COLOR_LIGHT_GREY;
-    private static final String WHITEBG = SET_BG_COLOR_WHITE;
-    private static final String NAVYBG = SET_BG_COLOR_NAVY;
+    private final String[] columns = {"a", "b", "c", "d", "e", "f", "g", "h"};
+    private final String[] rows = {"1", "2", "3", "4", "5", "6", "7", "8"};
+    private final String BLACKBG = SET_BG_COLOR_LIGHT_GREY;
+    private final String WHITEBG = SET_BG_COLOR_WHITE;
+    private final String NAVYBG = SET_BG_COLOR_NAVY;
+    private final String[] whitePieces = {WHITE_BISHOP, WHITE_KING, WHITE_KNIGHT, WHITE_PAWN, WHITE_QUEEN, WHITE_ROOK};
+    private final String[] blackPieces = {BLACK_BISHOP, BLACK_KING, BLACK_KNIGHT, BLACK_PAWN, BLACK_QUEEN, BLACK_ROOK};
+
 
     public String gameToUi(ChessBoard board, ChessGame.TeamColor team){
         String[] squares = boardToSquares(board);
@@ -47,11 +51,30 @@ public class GameUI {
 
     private String[] boardToSquares(ChessBoard board){
         String[] boardSquares = new String[100];
+        // TODO: break the board up into individual strings for each square (including the row/column labels) 
 //        for (int row = 1; row < 9; row++){
 //            for (int col = 1; col < 9; col++){
 //
 //            }
 //        }
         return boardSquares;
+    }
+
+    // converts a ChessPiece into its corresponding UI element
+    private String pieceToUI(ChessPiece piece){
+        if(piece == null){
+            return EMPTY;
+        }
+
+        String[] pieceUIs = piece.getTeamColor() == ChessGame.TeamColor.WHITE ?
+                whitePieces : blackPieces;
+        return switch(piece.getPieceType()){
+            case KING -> pieceUIs[1];
+            case QUEEN -> pieceUIs[4];
+            case BISHOP -> pieceUIs[0];
+            case KNIGHT -> pieceUIs[2];
+            case ROOK -> pieceUIs[5];
+            case PAWN -> pieceUIs[3];
+        };
     }
 }
