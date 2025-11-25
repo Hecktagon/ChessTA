333c4288891dda76af9c89e92ff594eac8ca4994
Hecktagon <tayler.c.hilton@gmail.com>
2025-11-25 13:26:17 -0700

    Working on websocket handler and connections manager

--- server/src/main/java/websocket/WebsocketHandler.java
+++ server/src/main/java/websocket/WebsocketHandler.java
@@ -1,22 +1,56 @@
 package websocket;
 
+import com.google.gson.Gson;
 import io.javalin.websocket.*;
+import org.eclipse.jetty.websocket.api.Session;
 import org.jetbrains.annotations.NotNull;
+import websocket.commands.UserGameCommand;
+
+import java.io.IOException;
 
 public class WebsocketHandler  implements WsConnectHandler, WsMessageHandler, WsCloseHandler {
+
+    private final WsConnectionsManager connections = new WsConnectionsManager();
+
+    @Override
+    public void handleConnect(@NotNull WsConnectContext wsCtx) throws Exception {
+        System.out.println("Websocket connected");
+        wsCtx.enableAutomaticPings();
+    }
+
+    @Override
+    public void handleMessage(@NotNull WsMessageContext wsCtx) throws Exception {
+        try {
+            UserGameCommand userGameCommand = new Gson().fromJson(wsCtx.message(), UserGameCommand.class);
+            switch (userGameCommand.getCommandType()) {
+                case CONNECT -> playerJoinsGame(wsCtx.session, userGameCommand);
+                case MAKE_MOVE -> playerMakesMove(wsCtx.session, userGameCommand);
+                case LEAVE -> playerLeaves(wsCtx.session, userGameCommand);
+                case RESIGN -> playerResigns(wsCtx.session, userGameCommand);
+            }
+        } catch (IOException ex) {
+            System.out.println("WARNING: " + ex.toString());
+        }
+    }
 
     @Override
     public void handleClose(@NotNull WsCloseContext wsCtx) throws Exception {
 
     }
 
-    @Override
-    public void handleConnect(@NotNull WsConnectContext wsCtx) throws Exception {
-        wsCtx.enableAutomaticPings();
+    private void playerJoinsGame(Session session, UserGameCommand command) throws IOException{
+
     }
 
-    @Override
-    public void handleMessage(@NotNull WsMessageContext wsCtx) throws Exception {
+    private void playerMakesMove(Session session, UserGameCommand command) throws IOException{
+
+    }
+
+    private void playerLeaves(Session session, UserGameCommand command) throws IOException{
+
+    }
+
+    private void playerResigns(Session session, UserGameCommand command) throws IOException{
 
     }
 }
--- server/src/main/java/websocket/WsConnectionsManager.java
+++ server/src/main/java/websocket/WsConnectionsManager.java
@@ -1,15 +1,49 @@
 package websocket;
 
+import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class WsConnectionsManager {
-    private final ConcurrentHashMap<Session, Session> connections = new ConcurrentHashMap<>();
+    // TODO: HashSet prevents duplicate sessions in a game, do we want that?
+    private final ConcurrentHashMap<Integer, HashSet<Session>> gameConnections = new ConcurrentHashMap<>();
 
     //TODO: will have to keep track of connections per game
 
-    public void add(Session session){
+    public void addSessionToGame(Session session, Integer gameID){
+        if(!gameConnections.containsKey(gameID)){
+            gameConnections.put(gameID, new HashSet<>());
+        }
+        gameConnections.get(gameID).add(session);
+    }
 
+    public void removeSessionFromGame(Session session, Integer gameID) throws IOException {
+        tryGetGameSessions(gameID).remove(session);
+    }
+
+    public void gameBroadcast(Integer gameID, ServerMessage message, Session root) throws IOException {
+        HashSet<Session> gameSessions = tryGetGameSessions(gameID);
+        for(Session session : gameSessions){
+            sendServerMessage(session, message);
+        }
+    }
+
+    private void sendServerMessage(Session session, ServerMessage message) throws IOException {
+        if (session.isOpen()) {
+            String messageJson = new Gson().toJson(message);
+            session.getRemote().sendString(messageJson);
+        }
+    }
+
+    private HashSet<Session> tryGetGameSessions(Integer gameID) throws IOException {
+        if (!gameConnections.containsKey(gameID)){
+            throw new IOException("No Such Game in Websocket.");
+        }
+        return gameConnections.get(gameID);
     }
 }
