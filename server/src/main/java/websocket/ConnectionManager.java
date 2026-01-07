c84a2147b3ffacc2c44310dc28bcb1734955b138
Hecktagon <tayler.c.hilton@gmail.com>
2026-01-07 14:25:26 -0700

    Starter implementation of ConnectionManager

--- server/src/main/java/websocket/ConnectionManager.java
+++ server/src/main/java/websocket/ConnectionManager.java
@@ -1,29 +1,41 @@
 package websocket;
 
+import com.google.gson.Gson;
+import errors.ResponseException;
 import org.eclipse.jetty.websocket.api.Session;
 import websocket.messages.ServerMessage;
 
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionManager {
-    public final ConcurrentHashMap<Session, Session> connections = new ConcurrentHashMap<>();
+    public final ConcurrentHashMap<Integer, HashSet<Session>> connections = new ConcurrentHashMap<>();
 
-    public void add(Session session) {
-        connections.put(session, session);
+    public void add(Integer gameID, Session session) {
+        if(!connections.containsKey(gameID)){
+            connections.put(gameID, new HashSet<>());
+        }
+        connections.get(gameID).add(session);
     }
 
-    public void remove(Session session) {
-        connections.remove(session);
+    public void remove(Integer gameID, Session session) throws ResponseException {
+        if(!connections.contains(gameID)){
+            throw new ResponseException(ResponseException.Type.SERVER_ERROR, "No such game ID.");
+        }
+        HashSet<Session> sessions = connections.get(gameID);
+        if(!sessions.contains(session)){
+            throw new ResponseException(ResponseException.Type.SERVER_ERROR, "Invalid session.");
+        }
+        sessions.remove(session);
     }
 
-    public void broadcast(Session excludeSession, ServerMessage notification) throws IOException {
-        String msg = notification.toString();
-        for (Session c : connections.values()) {
-            if (c.isOpen()) {
-                if (!c.equals(excludeSession)) {
-                    c.getRemote().sendString(msg);
+    public void broadcast(Integer gameID, ServerMessage message, Session excludeSession) throws IOException {
+        for (Session session : connections.get(gameID)) {
+            if (session.isOpen()) {
+                if (!session.equals(excludeSession)) {
+                    session.getRemote().sendString(new Gson().toJson(message));
                 }
             }
         }
